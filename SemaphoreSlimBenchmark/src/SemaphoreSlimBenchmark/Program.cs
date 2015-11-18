70148bf5bfff3d14f7b65bcbf0d1f24f5e3d8290
Alexandr Nikitin <nikitin.alexandr.a@gmail.com>
2015-11-18 12:06:41 +0200

    Tests are used

--- SemaphoreSlimBenchmark/src/SemaphoreSlimBenchmark/Program.cs
+++ SemaphoreSlimBenchmark/src/SemaphoreSlimBenchmark/Program.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using SemaphorePerfTests;
+
 namespace SemaphoreSlimBenchmark
 {
     public class Program
     {
         public void Main(string[] args)
         {
+            SemaphoreSlimPerfTest.RunTest();
+            BlockingCollectionPerfTest.RunTest();
         }
     }
 }
